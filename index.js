14d54c8f2d34760db935fcb33d8623ff20905f26
robert <robert.kibet@student.moringaschool.com>
2022-08-23 18:36:16 +0300

    phase-1-control-flow-lab

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
-}
-
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+  if (feet <= 400){
+    return 'This one is on me!'
+    }else if(feet >= 401 && feet < 2000){
+     return 'That will be twenty bucks.'
+    }else if(feet >= 2001 && feet < 2500){
+    return 'I will gladly take your thirty bucks.'
+    }else if(feet >= 2501){
+    return 'No can do.'
+    }
+  }
+  
+  
+  function ternaryCheckCity(destination){
+    // Write your code here!
+  
+    return (destination === 'NYC'? 'Ok, sounds good.' :"No go.")
+  }
+  
+  function switchOnCharmFromTip(tip){
+    // Write your code here!
+    switch(tip){
+      case 'generous':
+        return "Thank you so much.";
+      break;
+      case 'not as generous':
+        return "Thank you.";
+      break;
+      default:
+      return "Bye." 
+    }
+  }
